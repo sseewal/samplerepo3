950f702a1fc8ebafd3a105e541083224d335f894
Sahil Seewal <sseewal@amazon.com>
2022-04-30 01:49:29 +0530

    sss

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -4,4 +4,5 @@ yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 yum install -y nodejs 
 npm i -g pm2
-pm2 stop all
+pm2 stop all || true
+pm2 delete all || true
