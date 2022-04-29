bf2805d48f11a93bcdbfcbf11d0937d65cd926be
Sahil Seewal <sseewal@amazon.com>
2022-04-30 01:34:29 +0530

    ss

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 . /home/ec2-user/.bashrc
-yum install epel-release
-yum install npm
+yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+yum install -y nodejs 
 npm i -g pm2
 pm2 stop all
 pm2 delete all
